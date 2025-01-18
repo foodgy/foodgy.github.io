7c730ba91b9f7a452d0d38b5b2c7ff29b5489296
foodgy <foodgy@gmail.com>
2025-01-18 19:48:25 +0300

    added supscription form

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
             fontFamily: {
                 'sans-serif': ['Montserrat', 'sans-serif'],
                 'serif': ['Merriweather', 'serif'],
-            }            
+            },
+            boxShadow: {
+                'framed': '0 2px 15px 0 var(--shadow-color)',
+            }           
         },
     },
     plugins: []
