ceaf8439706e0c6f559b01ab3b6250cf7fa4c976
foodgy <foodgy@gmail.com>
2025-01-21 23:52:00 +0300

    added webp generation on build

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ gem 'jekyll'
 
 group :jekyll_plugins do
     gem 'jekyll-postcss-v2'
+    gem 'jekyll-webp'
     gem 'jekyll-feed'
     gem 'jekyll-sitemap'
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
                 'sans-serif': ['Montserrat', 'sans-serif'],
                 'serif': ['Merriweather', 'serif'],
             },
+            fontSize: {
+                '2.5xl': '1.75rem',
+                '4.5xl': '2.5rem',
+            },
+            lineHeight: {
+                '11': '2.75rem',
+            },
             boxShadow: {
                 'framed': '0 2px 15px 0 var(--shadow-color)',
             }           
